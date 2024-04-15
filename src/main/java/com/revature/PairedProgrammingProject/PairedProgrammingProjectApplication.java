bd36553a1943e5e56253951931b2d99f89c0fb8d
danielri02 <danielistre02@gmail.com>
2024-04-15 13:14:05 -0500

    init

---
+++ src/main/java/com/revature/PairedProgrammingProject/PairedProgrammingProjectApplication.java
@@ -0,0 +1,13 @@
+package com.revature.PairedProgrammingProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PairedProgrammingProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PairedProgrammingProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/revature/PairedProgrammingProject/PairedProgrammingProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.revature.PairedProgrammingProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PairedProgrammingProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
